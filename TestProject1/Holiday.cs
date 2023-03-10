ec10ab19d156571e75245f8a0f5caba0aa6b390d
Cola Chen <cola018@gmail.com>
2023-03-10 15:33:48 +0800

    feat: 建立Production Code

---
+++ TestProject1/Holiday.cs
@@ -0,0 +1,15 @@
+﻿namespace TestProject1;
+
+public class Holiday
+{
+    public string SayXmas()
+    {
+        var today = DateTime.Today;
+        if (today.Month ==12 && today.Day ==25)
+        {
+            return "Merry Xmas";
+        }
+
+        return "Today is not Xmas";
+    }
+}
---
+++ TestProject1/HolidayTests.cs
@@ -0,0 +1,11 @@
+namespace TestProject1;
+
+[TestClass]
+public class HolidayTests
+{
+    [TestMethod]
+    public void 今天是聖誕節()
+    {
+
+    }
+}
