1b0d85fb69b068b0e3fa69c09be8df1580fc00ec
Cola Chen <cola018@gmail.com>
2023-03-10 15:51:32 +0800

    test: 完成今天不是聖誕節的測試案例
    MyHoliday新增一個SetToday的方法，讓測試案例可以輸入日期

--- TestProject1/HolidayTests.cs
+++ TestProject1/HolidayTests.cs
@@ -12,12 +12,30 @@ public class HolidayTests
         var expected = "Merry Xmas";
         Assert.AreEqual(expected, actual);
     }
+
+    [TestMethod]
+    public void 今天不是聖誕節()
+    {
+        var target = new MyHoliday();
+        target.SetToday(new DateTime(2200,3,8));
+        var actual = target.SayXmas();
+
+        var expected = "Today is not Xmas";
+        Assert.AreEqual(expected, actual);
+    }
 }
 
 public class MyHoliday : Holiday
 {
+    private DateTime _today;
+
+    public void SetToday(DateTime today)
+    {
+        _today = today;
+    }
+    
     protected override DateTime GetToday()
     {
-        return new DateTime(1999, 12, 25);
+        return _today;
     }
 }
