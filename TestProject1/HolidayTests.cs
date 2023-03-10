dee23545f5a67eb125b658cf98357830ca5b227c
Cola Chen <cola018@gmail.com>
2023-03-10 15:45:10 +0800

    test: 完成今天是聖誕節的測試案例
    透過工具產生繼承Holiday的MyHoliday物件，並覆寫GetToday方法

--- TestProject1/HolidayTests.cs
+++ TestProject1/HolidayTests.cs
@@ -6,6 +6,18 @@ public class HolidayTests
     [TestMethod]
     public void 今天是聖誕節()
     {
+        var target = new MyHoliday();
+        var actual = target.SayXmas();
 
+        var expected = "Merry Xmas";
+        Assert.AreEqual(expected, actual);
+    }
+}
+
+public class MyHoliday : Holiday
+{
+    protected override DateTime GetToday()
+    {
+        return new DateTime(1999, 12, 25);
     }
 }
