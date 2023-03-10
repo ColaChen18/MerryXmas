87c1ccbe02dae32386803875e4f99b5a0329ea72
Cola Chen <cola018@gmail.com>
2023-03-10 16:16:21 +0800

    feat: 今天日期改用property和filed取得

--- TestProject1/Holiday.cs
+++ TestProject1/Holiday.cs
@@ -2,19 +2,28 @@
 
 public class Holiday
 {
+    private DateTime _today;
+    public DateTime Today
+    {
+        private get
+        {
+            if (_today == null)
+            {
+                _today = DateTime.Today;
+            }
+            return _today;
+        }
+        set => _today = value;
+    }
+
     public string SayXmas()
     {
-        var today = GetToday();
+        var today = Today;
         if (today.Month == 12 && today.Day == 25)
         {
             return "Merry Xmas";
         }
 
         return "Today is not Xmas";
-    }
-
-    private static DateTime GetToday()
-    {
-        return DateTime.Today;
     }
 }
