6d223fc58028f65b19ce62f5ed4d94216a4e0e4f
Cola Chen <cola018@gmail.com>
2023-03-10 21:27:36 +0800

    feat: 在測試程式裡註冊物件到 DI Container。只是註冊所以仍是紅燈

--- TestProject1/HolidayTests.cs
+++ TestProject1/HolidayTests.cs
@@ -1,3 +1,5 @@
+using Autofac;
+
 namespace TestProject1;
 
 [TestClass]
@@ -6,6 +8,15 @@ public class HolidayTests
     [TestMethod]
     public void 今天是聖誕節()
     {
+        var builder = new ContainerBuilder();
+        builder.RegisterType<TimeProvider>().As<ITimeProvider>();
+        builder.RegisterType<Holiday>();
+        var container = builder.Build();
 
+        using var scope = container.BeginLifetimeScope();
+        var target = scope.Resolve<Holiday>();
+        var actual = target.SayXmas();
+
+        Assert.AreEqual(true, actual);
     }
 }
