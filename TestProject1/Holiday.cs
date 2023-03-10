248127081f0e18564701cfdd6ebd7961b22415e0
Cola Chen <cola018@gmail.com>
2023-03-10 16:01:51 +0800

    feat: 從constructor注入

--- TestProject1/Holiday.cs
+++ TestProject1/Holiday.cs
@@ -2,19 +2,21 @@
 
 public class Holiday
 {
+    private readonly ITimeProvider _timeProvider;
+
+    public Holiday(ITimeProvider timeProvider)
+    {
+        _timeProvider = timeProvider;
+    }
+
     public string SayXmas()
     {
-        var today = GetToday();
+        var today = _timeProvider.GetToday();
         if (today.Month == 12 && today.Day == 25)
         {
             return "Merry Xmas";
         }
 
         return "Today is not Xmas";
-    }
-
-    private static DateTime GetToday()
-    {
-        return DateTime.Today;
     }
 }
