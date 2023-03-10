accdaaa7fdbfcb1b73d231b19d95120b124721e4
Cola Chen <cola018@gmail.com>
2023-03-10 16:03:39 +0800

    test: 完成今天是聖誕節test case

--- TestProject1/HolidayTests.cs
+++ TestProject1/HolidayTests.cs
@@ -6,7 +6,12 @@ public class HolidayTests
     [TestMethod]
     public void 今天是聖誕節()
     {
+        ITimeProvider timeProvider =  new StubTimeProvider();
 
+        var target = new Holiday(timeProvider);
+        var actual = target.SayXmas();
+        
+        Assert.AreEqual("Merry Xmas", actual);
     }
 }
 
