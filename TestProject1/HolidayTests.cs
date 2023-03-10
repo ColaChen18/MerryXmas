10b93c2cf45331e60e7c6295d87b5794d91767f0
Cola Chen <cola018@gmail.com>
2023-03-10 21:16:27 +0800

    feat: 套用NSubstitute

--- TestProject1/HolidayTests.cs
+++ TestProject1/HolidayTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
 
 namespace TestProject1;
 
@@ -11,9 +12,12 @@ public class HolidayTests
         var service = new ServiceCollection();
         service.AddScoped<ITimeProvider, TimeProvider>();
         service.AddSingleton<Holiday>();
-        var buildServiceProvider = service.BuildServiceProvider();
-        
-        var target = buildServiceProvider.GetService<Holiday>();
+        // var buildServiceProvider = service.BuildServiceProvider();
+        // var target = buildServiceProvider.GetService<Holiday>();
+
+        var timeProvider = Substitute.For<ITimeProvider>();
+        timeProvider.GetToday().Returns(new DateTime(1999, 12, 25));
+        var target = new Holiday(timeProvider);
         var actual = target.SayXmas();
         
         Assert.AreEqual("Merry Xmas", actual);
